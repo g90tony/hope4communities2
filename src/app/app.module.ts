ee5d7b97de74c988f19b82521c3707ddc0ae9371
Caleb Tony <calebmbugua@gmail.com>
2021-04-13 01:29:13 +0300

    added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { LandingSectionComponent } from './components/landing-section/landing-section.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent, NavbarComponent, LandingSectionComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
