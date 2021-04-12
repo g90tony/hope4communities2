f4a573cb38cd076a60af652a3439ea2bf272ff7f
Caleb Tony <calebmbugua@gmail.com>
2021-04-13 02:25:42 +0300

    updated router import and exports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './page/landing/landing-page.component';
 
-
+const routes: Routes = [{ path: '', component: LandingPageComponent }];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
