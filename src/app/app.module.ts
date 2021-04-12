ccbae330d86ac11cc9a53dbd6ba17bae5fa237e1
Caleb Tony <calebmbugua@gmail.com>
2021-04-13 02:26:28 +0300

    added landing sections and page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,17 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { LandingSectionComponent } from './components/landing-section/landing-section.component';
 import { AppRoutingModule } from './app-routing.module';
+import { LandingPageComponent } from './page/landing/landing-page.component';
+import { KnowusSectionComponent } from './components/knowus-section/knowus-section.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, LandingSectionComponent],
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    LandingSectionComponent,
+    LandingPageComponent,
+    KnowusSectionComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
