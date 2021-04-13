1926427e2e279aa6e8577b7dc54c2264e6b37562
Caleb Tony <calebmbugua@gmail.com>
2021-04-14 01:29:42 +0300

    added route to work page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingPageComponent } from './page/landing/landing-page.component';
+import { WorkPageComponent } from './page/work-page/work-page.component';
 
-const routes: Routes = [{ path: '', component: LandingPageComponent }];
+const routes: Routes = [
+  { path: '', component: LandingPageComponent },
+  { path: 'work', component: WorkPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
