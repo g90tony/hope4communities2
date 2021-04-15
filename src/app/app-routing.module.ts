0715aad565020ba65585a648ac82e842324d3060
Caleb Tony <calebmbugua@gmail.com>
2021-04-15 16:20:18 +0300

    added contacts into the router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactPageComponent } from './page/contact-page/contact-page.component';
 import { LandingPageComponent } from './page/landing/landing-page.component';
 import { WorkPageComponent } from './page/work-page/work-page.component';
 
 const routes: Routes = [
   { path: '', component: LandingPageComponent },
   { path: 'work', component: WorkPageComponent },
+  { path: 'contact', component: ContactPageComponent },
 ];
 
 @NgModule({
