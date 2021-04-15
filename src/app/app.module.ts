5063a27290273da011f771245dc5f079c0b7df90
Caleb Tony <calebmbugua@gmail.com>
2021-04-15 14:49:47 +0300

    registered new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { WorkPageComponent } from './page/work-page/work-page.component';
 import { WorkLandingSectionComponent } from './components/work-landing-section/work-landing-section.component';
 import { WorkPurposeSectionComponent } from './components/work-purpose-section/work-purpose-section.component';
 import { ContactComponent } from './page/contact/contact.component';
+import { ContactLandingComponent } from './components/contact-landing/contact-landing.component';
 
 const firebaseConfig = {
   apiKey: environment.apiKey,
@@ -39,6 +40,7 @@ const firebaseConfig = {
     WorkLandingSectionComponent,
     WorkPurposeSectionComponent,
     ContactComponent,
+    ContactLandingComponent,
   ],
   imports: [
     BrowserModule,
