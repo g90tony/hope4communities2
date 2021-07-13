d387ddb0ed1d0222cc99202a62907d41f3d74912
Caleb Tony <calebmbugua@gmail.com>
2021-07-13 19:49:52 +0300

    restructured component folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AngularFireModule } from '@angular/fire';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { LandingSectionComponent } from './components/landing-section/landing-section.component';
+import { NavbarComponent } from './components/global_components/navbar/navbar.component';
+import { LandingSectionComponent } from './components/index/landing-section/landing-section.component';
 import { AppRoutingModule } from './app-routing.module';
 import { LandingPageComponent } from './page/landing/landing-page.component';
-import { KnowusSectionComponent } from './components/knowus-section/knowus-section.component';
-import { FutureSectionComponent } from './components/future-section/future-section.component';
-import { TeamSectionComponent } from './components/team-section/team-section.component';
+import { KnowusSectionComponent } from './components/index/knowus-section/knowus-section.component';
+import { FutureSectionComponent } from './components/index/future-section/future-section.component';
+import { TeamSectionComponent } from './components/index/team-section/team-section.component';
 import { environment } from 'src/environments/environment';
 import { WorkPageComponent } from './page/work-page/work-page.component';
-import { WorkLandingSectionComponent } from './components/work-landing-section/work-landing-section.component';
-import { WorkPurposeSectionComponent } from './components/work-purpose-section/work-purpose-section.component';
+import { WorkLandingSectionComponent } from './components/work/work-landing-section/work-landing-section.component';
+import { WorkPurposeSectionComponent } from './components/work/work-purpose-section/work-purpose-section.component';
 import { ContactPageComponent } from './page/contact-page/contact-page.component';
-import { ContactLandingComponent } from './components/contact-landing/contact-landing.component';
+import { ContactLandingComponent } from './components/contact/contact-landing/contact-landing.component';
 
 const firebaseConfig = {
   apiKey: environment.apiKey,
