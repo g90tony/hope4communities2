b6e31d0d3792450bebd544fa535cd15e09c58593
Caleb Tony <calebmbugua@gmail.com>
2021-07-14 11:09:07 +0300

    added blog index landing section

---
+++ src/app/components/blog_index/landing-section copy/blog-landing-section.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'blog-landing-section',
+  templateUrl: './blog-landing-section.component.html',
+  styleUrls: ['./blog-landing-section.component.css']
+})
+export class BlogLandingSectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
