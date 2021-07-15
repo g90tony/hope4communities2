d04249cd196356df47ecd1dffec37a4084069cc4
Caleb Tony <calebmbugua@gmail.com>
2021-07-15 10:48:14 +0300

    added article landing

---
+++ src/app/section/blog-article/landing-section/article-landing-section.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BlogLandingSectionComponent } from './article-landing-section.component';
+
+describe('BlogLandingSectionComponent', () => {
+  let component: BlogLandingSectionComponent;
+  let fixture: ComponentFixture<BlogLandingSectionComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [BlogLandingSectionComponent],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BlogLandingSectionComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/section/blog-article/landing-section/article-landing-section.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'article-landing-section',
+  templateUrl: './article-landing-section.component.html',
+  styleUrls: ['./article-landing-section.component.css'],
+})
+export class BlogLandingSectionComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
