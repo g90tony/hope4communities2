ef4fd9e3149bf2a63509de2ebe006367edde4ff7
Caleb Tony <calebmbugua@gmail.com>
2021-07-15 13:55:40 +0300

    completed get all posts request

--- src/app/services/contentful.service.ts
+++ src/app/services/contentful.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
+import { createClient, Entry } from 'contentful';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ContentfulService {
+  private contentfulClient = createClient({
+    space: environment.space,
+    accessToken: environment.accessToken,
+  });
+  constructor() {}
 
-  constructor() { }
+  getProducts(query?: object): Promise<Entry<any>[]> {
+    return this.contentfulClient
+      .getEntries(
+        Object.assign(
+          {
+            content_type: environment.contentTypeIds.product,
+          },
+          query
+        )
+      )
+      .then((res) => res.items);
+  }
 }
