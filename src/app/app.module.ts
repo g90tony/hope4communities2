b517ca5992b87cab828a7acfd274b034bad34f61
Caleb Tony <calebmbugua@gmail.com>
2021-07-15 05:25:45 +0300

    updated imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AngularFireModule } from '@angular/fire';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/global_components/navbar/navbar.component';
-import { LandingSectionComponent } from './components/index/landing-section/landing-section.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LandingSectionComponent } from './section/index/landing-section/landing-section.component';
 import { AppRoutingModule } from './app-routing.module';
 import { LandingPageComponent } from './page/landing/landing-page.component';
-import { KnowusSectionComponent } from './components/index/knowus-section/knowus-section.component';
-import { FutureSectionComponent } from './components/index/future-section/future-section.component';
-import { TeamSectionComponent } from './components/index/team-section/team-section.component';
+import { KnowusSectionComponent } from './section/index/knowus-section/knowus-section.component';
+import { FutureSectionComponent } from './section/index/future-section/future-section.component';
+import { TeamSectionComponent } from './section/index/team-section/team-section.component';
 import { environment } from 'src/environments/environment';
 import { WorkPageComponent } from './page/work-page/work-page.component';
-import { WorkLandingSectionComponent } from './components/work/work-landing-section/work-landing-section.component';
-import { WorkPurposeSectionComponent } from './components/work/work-purpose-section/work-purpose-section.component';
+import { WorkLandingSectionComponent } from './section/work/work-landing-section/work-landing-section.component';
+import { WorkPurposeSectionComponent } from './section/work/work-purpose-section/work-purpose-section.component';
 import { ContactPageComponent } from './page/contact-page/contact-page.component';
-import { ContactLandingComponent } from './components/contact/contact-landing/contact-landing.component';
+import { ContactLandingComponent } from './section/contact/contact-landing/contact-landing.component';
 import { BlogIndexPageComponent } from './page/blog_index/blog-index-page.component';
-import { BlogLandingSectionComponent } from './components/blog_index/landing-section/blog-landing-section.component';
-import { WeeksBlogComponent } from './components/blog_index/weeks-blog/weeks-blog.component';
+import { BlogLandingSectionComponent } from './section/blog_index/landing-section/blog-landing-section.component';
+import { WeeksBlogComponent } from './section/blog_index/weeks-blog/weeks-blog.component';
+import { BlogItemComponent } from './components/blog-item/blog-item.component';
 
 const firebaseConfig = {
   apiKey: environment.apiKey,
@@ -47,6 +48,7 @@ const firebaseConfig = {
     BlogIndexPageComponent,
     BlogLandingSectionComponent,
     WeeksBlogComponent,
+    BlogItemComponent,
   ],
   imports: [
     BrowserModule,
