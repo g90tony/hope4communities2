85a45a0bba2a12186df58a55197479b8b3e8b7cd
Caleb Tony <calebmbugua@gmail.com>
2021-07-15 10:15:48 +0300

    added search results url

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BlogSearchResultsComponent } from './page/blog-search-results/blog-search-results.component';
 import { BlogIndexPageComponent } from './page/blog_index/blog-index-page.component';
 import { ContactPageComponent } from './page/contact-page/contact-page.component';
 import { LandingPageComponent } from './page/landing/landing-page.component';
@@ -10,6 +11,7 @@ const routes: Routes = [
   { path: 'work', component: WorkPageComponent },
   { path: 'contact', component: ContactPageComponent },
   { path: 'blog', component: BlogIndexPageComponent },
+  { path: 'blog/search/:search_query', component: BlogSearchResultsComponent },
 
   { path: '**', redirectTo: '/' },
 ];
