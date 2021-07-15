80868ef39fe5a42a91bdb73312923f294c23371b
Caleb Tony <calebmbugua@gmail.com>
2021-07-15 15:06:02 +0300

    completed blog article content

--- src/app/section/blog-article/blog-article-content/blog-article-content.component.ts
+++ src/app/section/blog-article/blog-article-content/blog-article-content.component.ts
@@ -1,12 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Entry } from 'contentful';
+import { documentToHtmlString } from '@contentful/rich-text-html-renderer';
+
+import { ContentfulService } from 'src/app/services/contentful.service';
+
 @Component({
   selector: 'blog-article-content',
   templateUrl: './blog-article-content.component.html',
   styleUrls: ['./blog-article-content.component.css'],
 })
 export class BlogArticleContentComponent implements OnInit {
-  constructor() {}
+  current_post: Entry<any>;
 
-  ngOnInit(): void {}
+  constructor(private bloGService: ContentfulService) {}
+
+  ngOnInit(): void {
+    this.bloGService.getBlogPost('6vHHR4WlDzzFr9DqsFQzVc').then((results) => {
+      this.current_post = results;
+      console.log(this.current_post);
+    });
+  }
+
+  returnHtmlFromRichText(richText) {
+    if (
+      richText === undefined ||
+      richText === null ||
+      richText.nodeType !== 'document'
+    ) {
+      return '<p>Error</p>';
+    }
+    return documentToHtmlString(richText);
+  }
 }
