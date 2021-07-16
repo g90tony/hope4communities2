2afe4da44015354b5b1b4b1059e0cf714cba6cc8
Caleb Tony <calebmbugua@gmail.com>
2021-07-16 08:27:12 +0300

    compeleted view article

--- src/app/page/blog-view-article/blog-view-article.component.ts
+++ src/app/page/blog-view-article/blog-view-article.component.ts
@@ -1,14 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Entry } from 'contentful';
+import { ContentfulService } from 'src/app/services/contentful.service';
 
 @Component({
   templateUrl: './blog-view-article.component.html',
-  styleUrls: ['./blog-view-article.component.css']
+  styleUrls: ['./blog-view-article.component.css'],
 })
 export class BlogViewArticleComponent implements OnInit {
+  current_post: Entry<any>;
 
-  constructor() { }
+  constructor(private bloGService: ContentfulService) {}
 
   ngOnInit(): void {
+    this.bloGService.getBlogPost('6vHHR4WlDzzFr9DqsFQzVc').then((results) => {
+      this.current_post = results;
+      console.log(this.current_post);
+    });
   }
-
 }
--- src/app/section/blog-article/landing-section/article-landing-section.component.ts
+++ src/app/section/blog-article/landing-section/article-landing-section.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'article-landing-section',
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArticleLandingSectionComponent implements OnInit {
   constructor() {}
+
+  @Input() image: any;
+  @Input() title: any;
+  @Input() date: any;
+  @Input() author: any;
 
   ngOnInit(): void {}
 }
