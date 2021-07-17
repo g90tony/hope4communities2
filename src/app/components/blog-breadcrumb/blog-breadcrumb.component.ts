d0500f2baafa081227e7a121a84abd1e42f5afd9
Caleb Tony <calebmbugua@gmail.com>
2021-07-17 22:27:40 +0300

    completed blog post breadcrumb

--- src/app/components/blog-breadcrumb/blog-breadcrumb.component.ts
+++ src/app/components/blog-breadcrumb/blog-breadcrumb.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-blog-breadcrumb',
+  selector: 'blog-breadcrumb',
   templateUrl: './blog-breadcrumb.component.html',
-  styleUrls: ['./blog-breadcrumb.component.css']
+  styleUrls: ['./blog-breadcrumb.component.css'],
 })
 export class BlogBreadcrumbComponent implements OnInit {
+  @Input() post_title;
+  @Input() post_id;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
