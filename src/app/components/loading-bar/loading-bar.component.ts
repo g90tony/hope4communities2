44afbec165134d7d2c4ad0a11bf6dcaac42e7230
Caleb Tony <calebmbugua@gmail.com>
2021-07-21 16:20:22 +0300

    added asset loader for landing page

--- src/app/components/loading-bar/loading-bar.component.ts
+++ src/app/components/loading-bar/loading-bar.component.ts
@@ -1,43 +1,12 @@
 import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-loading-bar',
+  selector: 'loading-bar-animation',
   templateUrl: './loading-bar.component.html',
   styleUrls: ['./loading-bar.component.css'],
 })
 export class LoadingBarComponent implements OnInit {
-  page_assets = [{ name: '', hasLoaded: false }];
-  asset_count = this.page_assets.length + 1;
-  progression = 0;
-
-  @Input() public hasLoaded: (param: any) => number;
-
-  public assetHasLoaded(asset_name: string) {
-    this.page_assets.forEach((asset) => {
-      if (asset.name == asset_name) {
-        asset.hasLoaded = true;
-      }
-    });
-
-    this.page_assets.forEach((asset) => {
-      if (asset.hasLoaded) {
-        this.progression++;
-      }
-    });
-
-    if (this.progression == this.asset_count) {
-      this.hasLoaded(true);
-    }
-  }
-
-  public registerAsset(new_asset: { name: string; hasLoaded: false }) {
-    if (this.page_assets.length < 1) {
-      this.page_assets[0].name = new_asset.name;
-      this.page_assets[0].hasLoaded = new_asset.hasLoaded;
-    } else {
-      this.page_assets.push(new_asset);
-    }
-  }
+  @Input() progression: number;
 
   constructor() {}
 
--- src/app/page/landing/landing-page.component.ts
+++ src/app/page/landing/landing-page.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { timeout } from 'rxjs/operators';
+import { AssetLoadingAnimationService } from 'src/app/services/asset-loading-animation.service';
 import { PageLoadingAnimationService } from 'src/app/services/page-loading-animation.service';
 
 @Component({
@@ -7,14 +9,57 @@ import { PageLoadingAnimationService } from 'src/app/services/page-loading-anima
   styleUrls: ['./landing-page.component.css'],
 })
 export class LandingPageComponent implements OnInit {
+  asset_progression: number;
+  assets_state: boolean;
   page_state: boolean;
-  constructor(private pageLoader: PageLoadingAnimationService) {
+
+  constructor(
+    private pageLoader: PageLoadingAnimationService,
+    private assetLoader: AssetLoadingAnimationService
+  ) {
     this.pageLoader.setLoadTrue();
     this.page_state = this.pageLoader.getPageState();
+
+    this.assets_state = false;
+    this.asset_progression = 0;
   }
 
   ngOnInit(): void {
     this.pageLoader.setLoadFalse();
     this.page_state = this.pageLoader.getPageState();
+
+    this.assetRegister();
   }
+
+  assetHasLoaded() {}
+
+  assetRegister = () => {
+    setTimeout(() => {
+      let HTML_assets = document.getElementsByTagName('img');
+      let assets = Array.from(HTML_assets);
+
+      assets.forEach((asset) => {
+        let new_asset = {
+          name: asset.id,
+          hasLoaded: false,
+        };
+
+        this.assetLoader.registerAsset({
+          name: asset.id,
+          hasLoaded: false,
+        });
+
+        asset.addEventListener('load', (event) => {
+          this.assetLoader.assetHasLoaded(new_asset.name);
+          this.asset_progression = this.assetLoader.getProgress();
+
+          if (this.asset_progression == 100) {
+            setTimeout(() => {
+              this.assets_state = true;
+            }, 1200);
+          }
+        });
+      });
+    }, 10);
+  };
 }
