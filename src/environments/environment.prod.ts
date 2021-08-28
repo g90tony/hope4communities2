61cf4bbb88c8eadc3686cad51e5185aa67ac728e
Caleb Tony <calebmbugua@gmail.com>
2021-08-28 21:16:24 +0300

    added envrionment files for deployment

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: false,
+
+  space: 'kf7e3i8xn9li',
+  accessToken: 'VIU9vLSOQR3j5V7ygHER4K1k6SDuoPyjQcXeqT7rRF4',
+
+  contentTypeIds: {
+    blogPosts: 'blogPost',
+  },
+};
